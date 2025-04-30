4f5ecf4efd670d07fa3229ed0b6195c5d3e04f36
pravda <theusernameistakentrywiki@gmail.com>
2025-04-29 19:46:24 -0700

    added unpack() for block data structure

--- Block.py
+++ Block.py
@@ -48,3 +48,39 @@ class Block:
 
         except struct.error as e:
             print(f"Error while packing block: {e}")
+
+    @classmethod
+    def unpack(cls, packed_data:bytes) -> 'Block':
+
+        block_format_string = "32s d 32s 12s 12s 12s I"
+
+        fixed_header_size = struct.calcsize(block_format_string)
+
+        if len(packed_data) < fixed_header_size:
+            raise ValueError(f"Packed data is shorter than minimum header length. minimum length: {fixed_header_size} actual length: {len(packed_data)}")
+
+        try:
+            fixed_header_bytes = packed_data[:fixed_header_size]
+            unpacked_fixed_data = struct.unpack(block_format_string, fixed_header_bytes)
+
+            prev_hash, timestamp, case_id, evidence_id, state, creator, owner, data_length = unpacked_fixed_data
+
+            data = packed_data[fixed_header_size:expected_total_size]
+
+            return cls(prev_hash,
+                       timestamp,
+                       case_id,
+                       evidence_id,
+                       state,
+                       creator,
+                       owner,
+                       data_length,
+                       data)
+
+        except struct.error as e:
+            print(f"Error while unpacking block: {e}")
+            raise
+        except ValueError as e:
+            print(f"Error during unpacking: {e}")
+            raise
+
